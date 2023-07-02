d5ddae580f3cc27e41b044720185e58589f27c19
Polyxen1a <maslowa@inbox.ru>
2023-07-02 21:32:03 +0300

    Initial commit

---
+++ src/test/java/com/example/functionalinterfacesandlambdav6/FunctionalInterfacesAndLambdaV6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.functionalinterfacesandlambdav6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalInterfacesAndLambdaV6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
